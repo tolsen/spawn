7db1f575eb0ea537ccd1cc319ea93c450080f3aa
Tim Olsen <tim@jux.com>
2012-08-29 18:42:57 -0400

    We actually need to disconnect/reconnect even with Mongoid 3
    Stop supporting mongoid 2

--- lib/spawn.rb
+++ lib/spawn.rb
@@ -199,22 +199,17 @@ module Spawn
     end
   end
 
-  def mongoid_connection_handling_required?
-    @@mongoid_connection_handling_required ||=
-      (defined?(Mongoid) && Mongoid::VERSION.match(/^(\d+)\./)[1].to_i < 3)
-  end
-
   def reconnect
     # get a new connection so the parent can keep the original one
     # Old spawn did a bunch of hacks inside activerecord here. There is
     # most likely a reason that this won't work, but I am dumb.
     ActiveRecord::Base.connection.reconnect! if defined? ActiveRecord
     # Connect to Mongodb if Mongoid is in use
-    Mongoid.database.connection.connect if mongoid_connection_handling_required?
+    Mongoid::Threaded.sessions[:default] = Mongoid::Sessions::Factory.default if defined? Mongoid
   end
 
   def disconnect    
     ActiveRecord::Base.connection_handler.clear_all_connections! if defined? ActiveRecord
-    Mongoid.database.connection.close if mongoid_connection_handling_required?
+    Mongoid.default_session.disconnect if defined? Mongoid
   end
 end
